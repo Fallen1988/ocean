928714d37e993586b28918e80a93316f2bbb1bd4
Orest Andrusyshyn <Fallen_2006@ukr.net>
2017-09-29 16:24:33 +0300

    +

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require('browser-sync').create();
+
+// Для спрайта з svg
+/*
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+
+gulp.task("symbols", function () {
+    return gulp.src("img/icons/*.svg")
+        .pipe(svgmin())
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename("symbols.svg"))
+        .pipe(gulp.dest("img"));
+});
+*/
+gulp.task("sass", function() {
+    gulp.src("sass/style.scss")      //звідки ми все починаємо
+        .pipe(sass())
+        .pipe(gulp.dest("css"))     // в яку папку зберегти результат
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+
+});
+
+gulp.task('watch',['browserSync'], function(){
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch("*.html").on("change", browserSync.reload);
+    // Other watchers
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        }
+    })
+});
+
